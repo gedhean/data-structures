b80f3202521ca81f7cc228514ec173df4e79df80
Gêdhean Alves Vieira <gedhean.alves@secrel.net.br>
2018-09-19 10:02:36 -0300

    .gitignore added and new methods included

--- tree.cpp
+++ tree.cpp
@@ -10,11 +10,18 @@ private:
 public:
   Node(int value);
   Node(int value, Node *leftChild, Node *rightChild);
+  int getValue();                 /* Node stored value */
+  void setLeftChild(int value);   /* Set Node left child  */
+  void setRightChild(int value);  /* Set Node right child  */
+  Node* getLeftChild();
+  Node* getRightChild();
 };
 
 Node::Node(int value)
 {
   this->value = value;
+  this->leftChild = nullptr;
+  this->rightChild = nullptr;
 }
 
 Node::Node(int value, Node *leftChild, Node *rightChild) {
@@ -23,13 +30,34 @@ Node::Node(int value, Node *leftChild, Node *rightChild) {
   this->rightChild = rightChild;
 }
 
+int Node::getValue() {
+  return this->value;
+}
+
+void Node::setLeftChild(int value) {
+  this->leftChild = new Node(value);
+}
+void Node::setRightChild(int value) {
+  this->rightChild = new Node(value);
+}
+
+Node* Node::getLeftChild() {
+  return this->leftChild;
+}
+Node* Node::getRightChild() {
+  return this->rightChild;
+}
+
 class Tree
 {
 private:
   Node *root;     /* Root Tree Node */
 public:
   Tree();
-  bool empty();
+  Tree(Node* n);
+  bool empty(); 
+  void print();
+  Node* getRoot() { return this->root; };
 };
 
 Tree::Tree()
@@ -37,14 +65,35 @@ Tree::Tree()
   this->root = nullptr;
 }
 
+Tree::Tree(Node* n) {
+  this->root = n;
+}
+
 bool Tree::empty() {
   return this->root == nullptr;
 }
 
+void printTree(Node* T) {
+  if (T == nullptr) {
+    return;
+  }
+  std::cout << "Value: " << T->getValue() << std::endl;
+  printTree(T->getLeftChild());
+  printTree(T->getRightChild());
+}
+
+void Tree::print() {
+  printTree(this->root);
+}
+
 int main(int argc, char const *argv[])
 {
-  /* code */
   std::cout << "Hello Trees /o\\" << std::endl;
+  /* code */
+  Tree T(new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3)));
+
+  T.print();
+  std::cout << "Tree is empty: " << T.empty() << std::endl;
 
   return 0;
 }
