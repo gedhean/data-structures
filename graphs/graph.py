94e0f36303134421579cac0e6babbe5cb2ca14c2
Gêdhean Alves Vieira <gedhean.alves@secrel.net.br>
2018-10-30 07:52:27 -0300

    Improve documentation

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,8 +1,14 @@
 class Vertex:
 	"""Graph vertex data structure"""
 	def __init__(self, id):
-		self.id = id
-		self.connectedTo = {}
+		'''
+		Create a new graph vertex.
+		type id: integer or string - unique vertex identifier
+		'''
+		self.id = id 			
+		# Dictinary of vertices connected with that vertex. { key-vertex id: value-weight }
+		# A vertex V is `connected to` that vertex if there is an edge from that vertex to V
+		self.connectedTo = {}	
 	
 	def __str__(self):
 		'Return the String representation of this Vertex'
@@ -11,40 +17,40 @@ class Vertex:
 	def addNeighbor(self, nbr, weight=0):
 		'''
 		Add a new Vertex to the list of neighbors of this vertex
-		type nbr: integer or string
-		type weight: number
+		type nbr: integer or string - vertex id
+		type weight: number			- edge weight
 		'''
 		self.connectedTo[nbr] = weight
 
 	def getId(self):
 		'''
 		Return the Vertex identifier
-		rtype: integer or string
+		rtype: integer or string - vertex id
 		'''
 		return self.id
 
 	def getNeighbors(self):
 		'''
-		Return a list of Vertex connected to this vertex
-		rtype: dict_keys
+		Return a list of vertices (id) connected to that vertex
+		rtype: dict_keys - vetices id
 		'''
 		return self.connectedTo.keys()
 
 	def getWeightOf(self, nbr):
 		'''
 		Return the weight of the given neighbor (nbr)
-		type nbr: integer or str
-		rtype: number
+		type nbr: integer or str - vertex id
+		rtype: number 			 - edge weight
 		'''
 		return self.connectedTo[nbr]
 
 
 
 class Graph:
-	"""Graph data structure"""
+	"""Weight Directed Graph (WDG)data structure"""
 	def __init__(self):
 		'''
-		Create a new empty directed weighted Graph
+		Create a new empty DWG
 		'''
 		self.vertices = {} 
 		self.numVertices = 0
@@ -60,7 +66,8 @@ class Graph:
 
 	def addVertice(self, key):
 		'''
-		Add a new Vertex to this graph
+		Add a new Vertex to that Graph
+		type key: integer or string - vertex id
 		'''
 		self.numVertices += 1
 		self.vertices[key] = Vertex(key)
@@ -68,7 +75,7 @@ class Graph:
 	def getVertice(self, key):
 		'''
 		Return the Vertex object of the given key
-		rtype: Vertex
+		rtype: integer or string - vertex id 
 		'''
 		if key in vertices:
 			return vertices[key]
@@ -76,7 +83,10 @@ class Graph:
 
 	def addEdge(self, fromVertex, toVertex, weight=0):
 		'''
-		Add a new directed edge from fromVertex to toVertex vetices with the given wreight 
+		Add a new directed edge from fromVertex to toVertex vetices with the given weight
+		type fromVertex: integer or string - vertex id 
+		type toVertex: 	 integer or string - vertex id 
+		type weight:	 integer 		   - edge weight
 		'''
 		if fromVertex not in self.vertices:
 			self.addVertice(fromVertex)
